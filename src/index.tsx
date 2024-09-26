0775946c51f384a0c836e4acaf32693e55f3bd98
Alla Novytska <anovytska2022u@gmail.com>
2024-09-26 15:52:21 +0200

    arbaiteBrauserPageOnly

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import Counter from './components/counter/Counter';
 import Feedback from './components/feedback/Feedback';
 import FetchDog from './components/fetchDog/FetchDog';
+import FetchCat from './components/fetchCat/FetchCat';
 import HomePage from './components/homepage/HomePage';
 import MyForm from './components/myForm/MyForm';
 import './index.css';
@@ -11,14 +12,15 @@ import NoPage from './components/noPage/NoPage';
 import Lesson01 from './lessons/lesson_01/Lesson01';
 import Lesson02 from './lessons/lesson_02/Lesson02';
 import Lesson03 from './lessons/lesson_03/Lesson03';
-import Lesson06 from './lessons/lesson_06/Lesson06';
-import Lesson05 from './lessons/lesson_05/Lesson05';
 import Lesson04 from './lessons/lesson_04/Lesson04';
+import Lesson05 from './lessons/lesson_05/Lesson05';
+import Lesson06 from './lessons/lesson_06/Lesson06';
 import Lesson07 from './lessons/lesson_07/Lesson07';
 import Lesson08 from './lessons/lesson_08/Lesson08';
 import Lesson09 from './lessons/lesson_09/Lesson09';
 import Lesson10 from './lessons/lesson_10/Lesson10';
 import Lesson11 from './lessons/lesson_11/Lesson11';
+
 
 
 const root = ReactDOM.createRoot(
@@ -35,6 +37,7 @@ root.render(
         {/* то есть в path могут прописаны любые значения одной строкой без пропуска - важно сопоставлять их со значениями в ссылке */}
         <Route element={<HomePage />} path='/' />
         <Route element={<FetchDog />} path='fetch-dog' />
+        <Route element={<FetchCat />} path='fetch-cat' />
         <Route element={<Counter />} path='counter' />
         <Route element={<MyForm />} path='my-form' />
         <Route element={<Feedback />} path='feedback' />
